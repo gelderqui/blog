d7ff5915e5912b264f9f9d9ba557567c659b39e1
Gelder Quiñónez <gelderqui@gmail.com>
2021-06-25 16:13:49 -0600

    datos de hosting

--- bd/conexion.php
+++ bd/conexion.php
@@ -2,9 +2,9 @@
     class Conexion{
         public static function Conectar(){
             define('servidor', 'localhost');
-            define('nombre_bd', 'crudphpvue');
-            define('usuario', 'root');
-            define('password', '');	
+            define('nombre_bd', 'u200878855_crudphpvue');
+            define('usuario', 'u200878855_gelder5');
+            define('password', 'h3T*i!12Wo?');	
             $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
             try{
                 $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);
